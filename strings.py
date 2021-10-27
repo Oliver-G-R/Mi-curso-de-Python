1a799e377d4a54437e82a31bfb2fe2dfae4a530a
OLiver Guerrero Ruiz <oliver.kali.hub@gmail.com>
2021-10-27 18:58:53 -0500

    init

---
+++ strings.py
@@ -0,0 +1,44 @@
+# Ejemplos de funciones con cadenas
+userName = "oliver_834_2"
+
+# usuario en mayusculas
+userNameU = userName.upper()
+
+# usuario en minusculas
+userNameL = userName.lower()
+
+# usuario en mayusculas y minusculas
+userNameC = userName.capitalize()
+
+# busca la posicion de una cadena y arroja el numero de la posicion
+'''
+    Se empieza a contar desde el 0
+    oliver_834_2 -> 11 indices
+    o -> 0
+    l -> 1
+    i -> 2
+    v -> 3
+    e -> 4
+    r -> 5
+    _ -> 6
+    8 -> 7
+    3 -> 8
+    4 -> 9
+    2 -> 10
+'''
+userNameFind = userName.find("_")
+
+# Remplaza todas las apariciones de una cadena en otra
+userNameR = userName.replace("_", "@") 
+
+# Cuenta el numero de caracteres de una cadena
+userNameCount = userName.count("_")
+
+# Separa una cadena en una lista
+userNameSplit = userName.split("_")
+
+# Cantidad de caracteres de una cadena
+userLength = len(userName)
+
+userNameIndex = userName.index("o")
+print(userNameIndex)
---
+++ vars.py
@@ -0,0 +1,46 @@
+#Forma de declarar variables
+
+'''
+    Hay distintas formas de declarar variables,
+    las formas mas comunes son las convenciones:
+
+    snake_case: nombre_variable
+    camelCase: nombreVariable
+    PascalCase: NombreVariable
+    UPPER_CASE: NOMBRE_VARIABLE
+
+    Las variables no pueden comenzar con numeros,
+    no pueden contener espacios o caracteres especiales,
+    por eso se suelen usar nombres descriptivos y claros,
+    siguiendo las convenciones de nombres de variables.
+
+'''
+
+
+'''
+    Normalmente al escribir una variable, se suele sigue 
+    el mismo patron, para que de esta forma se pueda
+    identificar que variable es.
+    En casos de variables con un valor importante alto, 
+    se suelen poner en mayusculas, para que se pueda
+    identificar facilmente.
+'''
+
+#Este valor no es una variable, es una constante, por lo tanto no cambia 
+
+USER_ID = 937363976293728764
+
+#Este valor es una variable, por lo tanto si puede ser cambiado 
+tokenUser = 'ijfew78g54ij0ey8gg4'
+
+#Segunda forma de asignar valores a variables
+'''
+    En python, se pueden asignar valores a variables
+    de forma mas sencilla, poniendo el orden separado de una coma 
+    y un espacio para asignar el valor a la variable en el orden
+    en el que se encuentran.
+'''
+
+nameMovie, yearMovie = 'El Padrino', 1972
+
+
